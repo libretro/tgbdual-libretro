b5c6c1c15e304d88be361150b78929484be89ccc
twinaphex <libretro@gmail.com>
2014-08-08 03:28:41 +0200

    (libretro) dmy_renderer - cleanups

--- libretro/dmy_renderer.cpp
+++ libretro/dmy_renderer.cpp
@@ -41,20 +41,23 @@ extern retro_environment_t environ_cb;
 
 bool _screen_2p_vertical = false;
 
-static inline bool button_pressed(int pad, int btn) {
-	static bool held[16] = {
-		false, false, false, false, false, false, false, false,
-		false, false, false, false, false, false, false, false,
-	};
-	if ( input_state_cb(pad, 1,0, btn) ) {
-		if ( ! held[btn] ) {
-			held[btn] = true;
-			return true;
-		}
-	} else {
-		held[btn] = false;
-	}
-	return false;
+static inline bool button_pressed(int pad, int btn)
+{
+   static bool held[16] = {
+      false, false, false, false, false, false, false, false,
+      false, false, false, false, false, false, false, false,
+   };
+   if ( input_state_cb(pad, 1,0, btn) )
+   {
+      if ( ! held[btn] )
+      {
+         held[btn] = true;
+         return true;
+      }
+   }
+   else
+      held[btn] = false;
+   return false;
 }
 
 dmy_renderer::dmy_renderer(int which)
@@ -63,9 +66,9 @@ dmy_renderer::dmy_renderer(int which)
 
 	retro_pixel_format pixfmt = RETRO_PIXEL_FORMAT_RGB565;
 	rgb565 = environ_cb(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &pixfmt);
-	if (rgb565) {
+
+	if (rgb565)
 		puts("Frontend supports RGB565; will use that instead of XRGB1555.");
-	}
 }
 
 dmy_renderer::~dmy_renderer()
@@ -74,27 +77,29 @@ dmy_renderer::~dmy_renderer()
 
 word dmy_renderer::map_color(word gb_col)
 {
-	if(rgb565) {
-		return ((gb_col&0x001f) << 11) |
-		       ((gb_col&0x03e0) <<  1) |
-		       ((gb_col&0x0200) >>  4) |
-		       ((gb_col&0x7c00) >> 10);
-	}
-	return ((gb_col&0x001f) << 10) |
-	       ((gb_col&0x03e0)      ) | 
-	       ((gb_col&0x7c00) >> 10);
+	if(rgb565)
+   {
+      return ((gb_col&0x001f) << 11) |
+         ((gb_col&0x03e0) <<  1) |
+         ((gb_col&0x0200) >>  4) |
+         ((gb_col&0x7c00) >> 10);
+   }
+   return ((gb_col&0x001f) << 10) |
+      ((gb_col&0x03e0)      ) | 
+      ((gb_col&0x7c00) >> 10);
 }
 
 word dmy_renderer::unmap_color(word gb_col)
 {
-	if(rgb565) {
-		return ((gb_col&0x001f) << 10) |
-		       ((gb_col&0x07c0) >>  1) |
-		       ((gb_col&0xf800) >> 11);
-	}
-	return ((gb_col&0x001f) << 10) |
-	       ((gb_col&0x03e0)      ) | 
-	       ((gb_col&0x7c00) >> 10);
+   if(rgb565)
+   {
+      return ((gb_col&0x001f) << 10) |
+         ((gb_col&0x07c0) >>  1) |
+         ((gb_col&0xf800) >> 11);
+   }
+   return ((gb_col&0x001f) << 10) |
+      ((gb_col&0x03e0)      ) | 
+      ((gb_col&0x7c00) >> 10);
 }
 
 void dmy_renderer::refresh() {
@@ -119,46 +124,53 @@ void dmy_renderer::refresh() {
 	(!!input_state_cb(which_gb,1,0, RETRO_DEVICE_ID_JOYPAD_LEFT)  ) << 6 |
 	(!!input_state_cb(which_gb,1,0, RETRO_DEVICE_ID_JOYPAD_RIGHT) ) << 7;
 
-	if (g_gb[1]) { // if dual gb mode
-		if (audio_2p_mode == 2) {
-			// mix down to one per channel (dual mono)
-			int16_t tmp_stream[SAMPLES_PER_FRAME*2];
-			this->snd_render->render(tmp_stream, SAMPLES_PER_FRAME);
-			for(int i = 0; i < SAMPLES_PER_FRAME; ++i) {
-				int l = tmp_stream[(i*2)+0], r = tmp_stream[(i*2)+1];
-				stream[(i*2)+which_gb] = int16_t( (l+r) / 2 );
-			}
-		} else if (audio_2p_mode == which_gb) {
-			// only play gb 0 or 1
-			this->snd_render->render(stream, SAMPLES_PER_FRAME);
-		}
-		if (which_gb == 1) {
-			// only do audio callback after both gb's are rendered.
-			audio_batch_cb(stream, SAMPLES_PER_FRAME);
+   if (g_gb[1])
+   { // if dual gb mode
+      if (audio_2p_mode == 2)
+      {
+         // mix down to one per channel (dual mono)
+         int16_t tmp_stream[SAMPLES_PER_FRAME*2];
+         this->snd_render->render(tmp_stream, SAMPLES_PER_FRAME);
+         for(int i = 0; i < SAMPLES_PER_FRAME; ++i)
+         {
+            int l = tmp_stream[(i*2)+0], r = tmp_stream[(i*2)+1];
+            stream[(i*2)+which_gb] = int16_t( (l+r) / 2 );
+         }
+      }
+      else if (audio_2p_mode == which_gb)
+      {
+         // only play gb 0 or 1
+         this->snd_render->render(stream, SAMPLES_PER_FRAME);
+      }
+      if (which_gb == 1)
+      {
+         // only do audio callback after both gb's are rendered.
+         audio_batch_cb(stream, SAMPLES_PER_FRAME);
 
-			/*
-			// switch screen orientation with X button
-			// this isn't enough to make retroarch change resolutions on the fly,
-			// even if the values returned by get_system_av_info change as well.
-			if ( button_pressed(0, RETRO_DEVICE_ID_JOYPAD_X) ) {
-				_screen_2p_vertical = ! _screen_2p_vertical;
-			}*/
-			// switch the playback mode with L/R
-			if ( button_pressed(0, RETRO_DEVICE_ID_JOYPAD_L) ) {
-				--audio_2p_mode;
-			} else if ( button_pressed(0, RETRO_DEVICE_ID_JOYPAD_R) ) {
-				++audio_2p_mode;
-			} else {
-				goto no_change;
-			}
-			audio_2p_mode &= 3;
-			memset(stream, 0, sizeof(stream));
-			environ_cb( RETRO_ENVIRONMENT_SET_MESSAGE,
-			            &audio_2p_mode_descriptions[audio_2p_mode] );
-		no_change:
-			;
-		}
-	} else {
+         /*
+         // switch screen orientation with X button
+         // this isn't enough to make retroarch change resolutions on the fly,
+         // even if the values returned by get_system_av_info change as well.
+         if ( button_pressed(0, RETRO_DEVICE_ID_JOYPAD_X) ) {
+         _screen_2p_vertical = ! _screen_2p_vertical;
+         }*/
+         // switch the playback mode with L/R
+         if (button_pressed(0, RETRO_DEVICE_ID_JOYPAD_L))
+            --audio_2p_mode;
+         else if (button_pressed(0, RETRO_DEVICE_ID_JOYPAD_R))
+            ++audio_2p_mode;
+         else
+            goto no_change;
+         audio_2p_mode &= 3;
+         memset(stream, 0, sizeof(stream));
+         environ_cb( RETRO_ENVIRONMENT_SET_MESSAGE,
+               &audio_2p_mode_descriptions[audio_2p_mode] );
+no_change:
+         ;
+      }
+   }
+   else
+   {
 		this->snd_render->render(stream, SAMPLES_PER_FRAME);
 		audio_batch_cb(stream, SAMPLES_PER_FRAME);
 	}
@@ -170,70 +182,76 @@ int dmy_renderer::check_pad()
 	return pad_state;
 }
 
-void dmy_renderer::render_screen(byte *buf,int width,int height,int depth) {
-	static byte joined_buf[160*144*2*2]; // two screens' worth of 16-bit data
-	const int half = sizeof(joined_buf)/2;
-	int pitch = width*((depth+7)/8);
-	if(g_gb[1]) { // are we running two gb's?
-		if(_screen_2p_vertical) {
-			memcpy(joined_buf + which_gb*half, buf, half);
-			if(which_gb == 1) {
-				video_cb(joined_buf, width, height*2, pitch);
-			}
-		} else {
-			for (int row = 0; row < height; ++row) {
-				memcpy(joined_buf + pitch*(2*row + which_gb), buf+pitch*row, pitch);
-			}
-			if(which_gb == 1) {
-				video_cb(joined_buf, width*2, height, pitch*2);
-			}
-		}
-	} else {
-		video_cb(buf, width, height, pitch);
-	}
+void dmy_renderer::render_screen(byte *buf,int width,int height,int depth)
+{
+   static byte joined_buf[160*144*2*2]; // two screens' worth of 16-bit data
+   const int half = sizeof(joined_buf)/2;
+   int pitch = width*((depth+7)/8);
+
+   if(g_gb[1])
+   {
+      // are we running two gb's?
+      if(_screen_2p_vertical)
+      {
+         memcpy(joined_buf + which_gb*half, buf, half);
+         if(which_gb == 1)
+            video_cb(joined_buf, width, height*2, pitch);
+      }
+      else
+      {
+         for (int row = 0; row < height; ++row)
+            memcpy(joined_buf + pitch*(2*row + which_gb), buf+pitch*row, pitch);
+         if(which_gb == 1)
+            video_cb(joined_buf, width*2, height, pitch*2);
+      }
+   }
+   else
+      video_cb(buf, width, height, pitch);
 }
 
 byte dmy_renderer::get_time(int type)
 {
-	dword now=fixed_time-cur_time;
+   dword now = fixed_time-cur_time;
 
-	switch(type){
-	case 8: // second
-		return (byte)(now%60);
-	case 9: // minute
-		return (byte)((now/60)%60);
-	case 10: // hour
-		return (byte)((now/(60*60))%24);
-	case 11: // day (L)
-		return (byte)((now/(24*60*60))&0xff);
-	case 12: // day (H)
-		return (byte)((now/(256*24*60*60))&1);
-	}
-	return 0;
+   switch(type)
+   {
+      case 8: // second
+         return (byte)(now%60);
+      case 9: // minute
+         return (byte)((now/60)%60);
+      case 10: // hour
+         return (byte)((now/(60*60))%24);
+      case 11: // day (L)
+         return (byte)((now/(24*60*60))&0xff);
+      case 12: // day (H)
+         return (byte)((now/(256*24*60*60))&1);
+   }
+   return 0;
 }
 
 void dmy_renderer::set_time(int type,byte dat)
 {
-	dword now=fixed_time;
-	dword adj=now-cur_time;
+   dword now = fixed_time;
+   dword adj = now - cur_time;
 
-	switch(type){
-	case 8: // second
-		adj=(adj/60)*60+(dat%60);
-		break;
-	case 9: // minute
-		adj=(adj/(60*60))*60*60+(dat%60)*60+(adj%60);
-		break;
-	case 10: // hour
-		adj=(adj/(24*60*60))*24*60*60+(dat%24)*60*60+(adj%(60*60));
-		break;
-	case 11: // day (L)
-		adj=(adj/(256*24*60*60))*256*24*60*60+(dat*24*60*60)+(adj%(24*60*60));
-		break;
-	case 12: // day (H)
-		adj=(dat&1)*256*24*60*60+(adj%(256*24*60*60));
-		break;
-	}
-	cur_time=now-adj;
+   switch(type)
+   {
+      case 8: // second
+         adj = (adj/60)*60+(dat%60);
+         break;
+      case 9: // minute
+         adj = (adj/(60*60))*60*60+(dat%60)*60+(adj%60);
+         break;
+      case 10: // hour
+         adj = (adj/(24*60*60))*24*60*60+(dat%24)*60*60+(adj%(60*60));
+         break;
+      case 11: // day (L)
+         adj = (adj/(256*24*60*60))*256*24*60*60+(dat*24*60*60)+(adj%(24*60*60));
+         break;
+      case 12: // day (H)
+         adj = (dat&1)*256*24*60*60+(adj%(256*24*60*60));
+         break;
+   }
+   cur_time = now - adj;
 }
 
